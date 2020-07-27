052a61cb86f1767a21d3a0cf196d856df127584a
Lucas-Ed <lucas_tranpo@YAHOO.com.br>
2020-07-26 23:12:50 -0300

    primeiros arqioos sql

---
+++ CRIANDO TABELA TESTE PARA TESTAR A CONEXÃO DO SCRIPT EM PYTHON-query.sql
@@ -0,0 +1,11 @@
+
+--CRIANDO TABELA TESTE PARA TESTAR A CONEXÃO DO SCRIPT EM Python
+CREATE TABLE TBL_TESTE(
+       NUMERO INT
+)
+
+INSERT INTO TBL_TESTE VALUES (1)
+
+
+--pegando os dados da tabela e ecibindo na tela
+---select * from Projeto.dbo.TBL_TESTE
